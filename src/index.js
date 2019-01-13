047b4f30fae13add918028127fb1f9759632a7c3
junohe <junohe@tencent.com>
2019-01-13 12:57:46 +0800

    v2.0 简单的编译（箭头函数）

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
-// const add = {
-//     extra: 2
-// };
-//
-// console.log('init ', {
-//     ...add,
-//     name: 1
-// });
-
 import a from './a.js';
-// import b from './b.js';
-
 console.log('hello', a);
-// console.log('hello', b);
 require.ensure('./b', function () {
-    let s = require('./b');
+    let moduleB = require('./b');
+    console.log('hello', moduleB);
+});
 
-})
+const ArrowFunction = () => 'this is ArrowFunction';
 

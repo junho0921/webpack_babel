483eb48d6f864f35c5db814ec67a01436da468cc
junohe <junohe@tencent.com>
2019-01-19 17:51:59 +0800

    API.13.0 Symbols

---
+++ src/es6_api/Symbols.js
@@ -0,0 +1,21 @@
+(function() {
+
+    // module scoped symbol
+    var key = Symbol("key");
+
+    function MyClass(privateData) {
+        this[key] = privateData;
+    }
+
+    MyClass.prototype = {
+        doStuff: function() {
+            // ... this[key] ...
+        }
+    };
+
+    // Limited support from Babel, full support requires native implementation.
+    typeof key === "symbol"
+})();
+
+var c = new MyClass("hello")
+c["key"] === undefined
